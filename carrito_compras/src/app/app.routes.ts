2c07f1500a7fbbb5db80661eda2408f7ed189e6e
Javier Alejandro Mazariegos Godoy <61554803+Javier-Mazariegos@users.noreply.github.com>
2025-01-18 13:48:55 -0600

    Creación componente lista productos
    Se creó el componente lista productos, en el cuál se mostrarán todos los productos que se consuman al API.

--- carrito_compras/src/app/app.routes.ts
+++ carrito_compras/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListaProductosComponent } from './components/productos/lista-productos/lista-productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ListaProductosComponent},
+    {path: '**', pathMatch: 'full' , redirectTo: ''} //Si el usuario ingresa una ruta incorrecta, se redirige al componente principal
+];
